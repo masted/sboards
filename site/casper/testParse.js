4fef7222080b4676a34a670ab386289d61799717
masted <masted311@gmail.com>
2014-08-19 15:52:54 +0400

    Commit was made from server localhost by ci/push

--- site/casper/testParse.js
+++
@@ -1,5 +0,0 @@
-var vkGroupSearch = new (require('vkGroupSearch'))(require('casper').create(), require('vkParserArgs'));
-vkGroupSearch.keyword = {
-  keyword: 'абсурд'
-};
-vkGroupSearch.auth(vkGroupSearch.startParse.bind(vkGroupSearch));
